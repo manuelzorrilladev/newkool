acc4daffef0a494a23ebe8e1ee2b1320feb8eb0c
Manuel Zorrilla <manuelzorrilladev@gmail.com>
2025-06-25 15:02:37 -0400

    Updated main slider

--- resources/js/Custom/MainSlider.vue
+++ resources/js/Custom/MainSlider.vue
@@ -1,3 +1,5 @@
+
+
 <script setup>
 import { computed, onMounted, ref, useTemplateRef, watch } from 'vue';
 import PrimaryButton from '../Components/PrimaryButton.vue'
@@ -35,7 +37,7 @@ function autoPlay() {
 // setInterval(autoPlay,4000)
 
 function update(type) {
-    // changeNested()
+    changeNested()
     if (type == 1) {
         if (validator.value < sliderV.value.length - 1) {
             validator.value = validator.value + 1
@@ -74,7 +76,7 @@ function changeNested() {
     nestedValidator.value = 0
     console.log("nested");
     const interval = setInterval(() => {
-        if (nestedValidator.value < 4) {
+        if (nestedValidator.value < 5) {
             nestedValidator.value++
         } else {
             clearInterval(interval)
@@ -94,7 +96,7 @@ onMounted(() => {
     <section>
      
         <div ref="el" class=" w-full h-fit flex items-center justify-center mt-10">
-            <div class="flex relative justify-start w-11/12 h-full overflow-hidden bg-newkool-red rounded-xl">
+            <div class="flex relative  justify-start border w-11/12 rounded-xl h-full overflow-hidden bg-newkool-red">
                 <div class="absolute top-1/2 z-10 left-4 cursor-pointer" @click="update(-1)">
                     <font-awesome-icon :icon="['fas', 'caret-left']"
                         class="text-3xl text-newkool-red hover:text-white duration-200 hover:bg-newkool-red p-3 rounded-lg" />
@@ -108,11 +110,36 @@ onMounted(() => {
 
                 </div>
 
-               
+                
 
                 <div class="absolute w- duration-500" :class="checkSlider3(3)">
-                    
-                    <img src="/assets/banner-images/banner-congelador.png" alt="congelador" class="hidden md:block">
+                    <Transition name="slide-fade">
+
+                        <div v-if="validator == 3"
+                            class="absolute z-0 flex flex-col w-full h-full items-center md:items-start md:justify-center pt-4 md:pt-0 md:left-16 mt-4">
+
+                            <h2 :class="nestedValidator >= 1 ? 'opacity-100 -translate-y-6' : 'opacity-0 translate-y-0'"
+                                class="text-gray-600 font-light md:text-4xl lg:text-5xl  duration-300  ">Almacenamiento Inteligente
+                            </h2>
+
+                            <h2 :class="nestedValidator >= 2 ? 'opacity-100 -translate-y-6' : 'opacity-0 translate-y-0'"
+                                class="text-newkool-red text-3xl md:text-7xl lg:text-8xl font-extrabold md:my-2  duration-300  ">
+                                 Frescura Garantizada
+                            </h2>
+
+                            <h2 :class="nestedValidator >= 3 ? 'opacity-100 -translate-y-6' : 'opacity-0 translate-y-0'"
+                                class="text-gray-600 font-light text-center md:text-left text-sm md:text-xl duration-300  w-10/12 md:w-full  ">Elige uno de nuestros congeladores, con tecnología avanzada y un diseño que optimiza el espacio, para mantener tus alimentos en perfectas condiciones por más tiempo.</h2>
+
+                        
+
+                            <PrimaryButton
+                                :class="nestedValidator >= 4 ? 'opacity-100 -translate-y-6' : 'opacity-0 translate-y-0'"
+                                path="/linea-blanca/Cocinas" class="md:mt-5 nested5 duration-300 ">VER MÁS
+                            </PrimaryButton>
+                        </div>
+
+                    </Transition>
+                     <img src="/assets/banner-images/banner-congelador.png" alt="congelador" class="hidden md:block">
                     <img src="/assets/banner-images/banner-congelador-mobile.png" alt="congelador"
                         class="block md:hidden w-full">
 
@@ -121,24 +148,98 @@ onMounted(() => {
                 </div>
 
                 <div class="absolute w-full duration-500" :class="checkSlider3(2)">
-               
-                    <img src="/assets/banner-images/banner-cocina.png" alt="cocina-banner" class="hidden md:block">
+                    <Transition name="slide-fade">
+
+                          <div v-if="validator == 2"
+                            class="absolute z-0 flex flex-col w-full h-full items-center md:items-start md:justify-center pt-4 md:pt-0 md:left-16 mt-4">
+
+                            <h2 :class="nestedValidator >= 1 ? 'opacity-100 -translate-y-6' : 'opacity-0 translate-y-0'"
+                                class="text-gray-600 font-light md:text-4xl lg:text-5xl  duration-300  ">Comodidad y Diseño
+                            </h2>
+
+                            <h2 :class="nestedValidator >= 2 ? 'opacity-100 -translate-y-6' : 'opacity-0 translate-y-0'"
+                                class="text-newkool-red text-3xl md:text-7xl lg:text-8xl font-extrabold md:my-2  duration-300  ">
+                                En un Solo Lugar
+                            </h2>
+
+                            <h2 :class="nestedValidator >= 3 ? 'opacity-100 -translate-y-6' : 'opacity-0 translate-y-0'"
+                                class="text-gray-600 font-light text-center md:text-left  md:text-xl duration-300  w-2/3 md:w-full  ">Elegí entre nuestras cocinas. <br>Pensadas para ofrecerte la mejor experiencia, eficiencia y estilo que mejoran tu calidad de vida.</h2>
+
+                        
+
+                            <PrimaryButton
+                                :class="nestedValidator >= 4 ? 'opacity-100 -translate-y-6' : 'opacity-0 translate-y-0'"
+                                path="/linea-blanca/Cocinas" class="md:mt-5 nested5 duration-300 ">VER MÁS
+                            </PrimaryButton>
+                        </div>
+
+                    </Transition>
+                     <img src="/assets/banner-images/banner-cocina.png" alt="cocina-banner" class="hidden md:block">
                     <img src="/assets/banner-images/banner-cocina-mobile.png" alt="cocina-banner"
                         class="block md:hidden w-full">
                 </div>
 
                 <div class="absolute w-full duration-500" :class="checkSlider3(1)">
-                   
+                    <Transition name="slide-fade">
+
+                        <div v-if="validator == 1"
+                            class="absolute z-0 flex flex-col w-full h-full items-start md:justify-center pt-4 md:pt-0 left-4 md:left-16 mt-4">
+
+                            <h2 :class="nestedValidator >= 1 ? 'opacity-100 -translate-y-6' : 'opacity-0 translate-y-0'"
+                                class="text-gray-600 font-light md:text-4xl lg:text-5xl  duration-300  ">Tu producto
+                            </h2>
+
+                            <h2 :class="nestedValidator >= 2 ? 'opacity-100 -translate-y-6' : 'opacity-0 translate-y-0'"
+                                class="text-newkool-red text-3xl md:text-7xl lg:text-8xl font-extrabold md:my-2  duration-300  ">
+                                Siempre Fresco</h2>
+
+                            <h2 :class="nestedValidator >= 3 ? 'opacity-100 -translate-y-6' : 'opacity-0 translate-y-0'"
+                                class="text-gray-600 font-light  text-sm md:text-xl duration-300  w-1/2 md:w-full  ">Exhibidoras refrigeradas para helados y jugos, con diseño atractivo y tecnología de enfriamiento eficiente. <br><strong>¡Mantén tus productos frescos y al alcance de todos!.</strong> </h2>
+
+                        
+
+                            <PrimaryButton
+                                :class="nestedValidator >= 4 ? 'opacity-100 -translate-y-6' : 'opacity-0 translate-y-0'"
+                                path="/linea-blanca/Exhibidoras-vitrinas" class="md:mt-5 nested5 duration-300 ">VER MÁS
+                            </PrimaryButton>
+                        </div>
+
+                    </Transition>
                     <img src="/assets/banner-images/banner-exibidora.png" alt="exibidora-newkool"
                         class="hidden md:block">
                     <img src="/assets/banner-images/banner-exibidora-mobile.png" alt="exibidora-newkool"
                         class="block md:hidden w-full">
 
 
+
                 </div>
 
                 <div class="absolute w-full  duration-500" :class="checkSlider3(0)">
-                   
+                    <Transition name="slide-fade">
+
+                        <div v-if="validator == 0"
+                            class="absolute z-0 flex flex-col w-full h-full items-center md:items-start md:justify-center pt-4 md:pt-0 md:left-16 mt-4">
+
+                            <h2 :class="nestedValidator >= 1 ? 'opacity-100 -translate-y-6' : 'opacity-0 translate-y-0'"
+                                class="text-gray-600 font-light md:text-4xl lg:text-5xl  duration-300  ">Sonido Claro 
+                            </h2>
+
+                            <h2 :class="nestedValidator >= 2 ? 'opacity-100 -translate-y-6' : 'opacity-0 translate-y-0'"
+                                class="text-newkool-red text-2xl md:text-7xl lg:text-8xl font-extrabold md:my-2  duration-300  ">
+                                Potencia Pura</h2>
+
+                            <h2 :class="nestedValidator >= 3 ? 'opacity-100 -translate-y-6' : 'opacity-0 translate-y-0'"
+                                class="text-gray-600 font-light text-center md:text-left text-xs md:text-xl duration-300  w-2/3 md:w-full">Bocinas con tecnología avanzada para una experiencia auditiva única. <br><strong>¡No te lo puedes perder!</strong></h2>
+
+                            <p :class="nestedValidator >= 4 ? 'opacity-100 -translate-y-6' : 'opacity-0 translate-y-0'"
+                                class="w-1/3 text-gray-600  mt-2 hidden md:block  duration-300  text-xl"></p>
+
+                            <PrimaryButton
+                                :class="nestedValidator >= 5 ? 'opacity-100 -translate-y-6' : 'opacity-0 translate-y-0'"
+                                path="/linea-blanca/Audio" class="md:mt-5 nested5 duration-300 ">VER MÁS
+                            </PrimaryButton>
+                        </div>
+                    </Transition>
                     <img src="/assets/banner-images/banner-bocina.png" alt="bocina-banner" class="hidden md:block">
                     <img src="/assets/banner-images/banner-bocina-mobile.png" alt="bocina-banner"
                         class="block md:hidden w-full">
@@ -177,3 +278,7 @@ onMounted(() => {
     opacity: 0;
 }
 </style>
+
+
+
+
--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -1,55 +1,196 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
 import MainTemplate from '../Layouts/MainTemplate.vue';
+import { useTemplateRef } from 'vue';
+import { useElementVisibility } from '@vueuse/core';
 
-import VideoBanner from '../Custom/VideoBanner.vue';
-
-
+const target1 = useTemplateRef("target1")
+const border1 = useTemplateRef("border1")
+const isTarget1Visible = useElementVisibility(target1)
+const isBorder1Visible = useElementVisibility(border1)
+const target2 = useTemplateRef("target2")
+const border2 = useTemplateRef("border2")
+const isTarget2Visible = useElementVisibility(target2)
+const isBorder2Visible = useElementVisibility(border2)
+const target3 = useTemplateRef("target3")
+const border3 = useTemplateRef("border3")
+const isTarget3Visible = useElementVisibility(target3)
+const isBorder3Visible = useElementVisibility(border3)
+const target4 = useTemplateRef("target4")
+const border4 = useTemplateRef("border4")
+const isTarget4Visible = useElementVisibility(target4)
+const isBorder4Visible = useElementVisibility(border4)
 
 
 </script>
 
 <template>
-        <MainTemplate class="bg-white">
+    <MainTemplate class="bg-white">
 
-            <Head>
-                <title>Somos Newkool</title>
-                <meta name="description" content="Newkoolamerica.com">
-            </Head>
+        <Head>
+            <title>Somos Newkool</title>
+            <meta name="description" content="Newkoolamerica.com">
+        </Head>
 
-            <VideoBanner />
+        <section class="relative flex justify-center w-full rounded-xl mt-4 mb-8">
+            <img src="/assets/route-images/somos-newkool/banner-somos-newkool.jpg" alt=""
+                class="w-11/12 hidden md:block rounded-xl">
+            <img src="/assets/route-images/somos-newkool/banner-somos-newkool-mobile.jpg" alt=""
+                class="w-11/12  block md:hidden rounded-xl">
+            <header class="absolute top-[5%] md:top-[40%] md:left-36 rounded-xl bg-white p-6">
+                <h2 class="text-newkool-red text-xl  md:text-3xl lg:text-5xl font-bold">Somos Newkool</h2>
+                <p class="md:text-xl font-light">Calidad Innovadora a tu Alcance.</p>
+            </header>
+        </section>
 
-            <div class="relative z-40 bg-white">
-                <header class="text-center py-20 flex flex-col items-center justify-center">
-                    <h2 class="font-bold text-4xl text-newkool-red">¿Quiénes somos?</h2>
-                    <p class="w-3/4 text-gray-700 text-lg">Somos una empresa que está comprometida con la mejora de la vida de
-                        los
-                        venezolanos. Ofrecemos productos de alta calidad que son duraderos, eficientes y fáciles de usar.</p>
-                </header>
-
+        <section class="flex items-center justify-center">
+            <hr class="border-black w-11/12">
+        </section>
+        <main class="w-full overflow-hidden">
+            <section 
+                ref="target1"
+                :class="isTarget1Visible ? 'opacity-100 translate-x-0':'opacity-0 -translate-x-full'"
+                class="flex justify-center py-20 duration-700">
+                <div
                 
-                <section class="w-full pb-20  flex items-center justify-center">
-                    <div class="w-3/4 flex items-center justify-center gap-8">
-                        <header>
-                            <h2 class="text-center text-2xl font-semibold text-newkool-red">Misión</h2>
-                            <p class="text-justify text-gray-700">Mejorar la vida de nuestros clientes a través de
-                                electrodomésticos innovadores y de alta calidad, y brindar un servicio excepcional que garantice
-                                su satisfacción y fidelidad hacia nuestra marca.</p>
-                        </header>
-                        <header>
-                            <h2 class="text-center text-2xl font-semibold text-newkool-red">Visión</h2>
-                            <p class="text-justify text-gray-700">Ser la marca líder en electrodomésticos, reconocida por la
-                                calidad, innovación y diseño de nuestros productos, así como por nuestra excelencia en el
-                                servicio al cliente.</p>
-                        </header>
+                class="flex flex-col md:flex-row items-center w-11/12">
+                    <div ref="border1" class="h-full w-0.5"></div>
+                    <header class="md:w-1/2 space-y-5">
+                        <p class="font-bold text-newkool-red">Sobre nosotros</p>
+                        <h1 class="font-bold text-newkool-red text-5xl">¿Quiénes somos?</h1>
+                        <p class="font-light md:w-2/3 text-gray-700">En Newkool, somos más que una marca de
+                            electrodomésticos; somos un compromiso con la mejora de la vida en cada hogar venezolano.
+                            Nos dedicamos a ofrecer productos que no solo cumplen, sino que superan las expectativas en
+                            términos de durabilidad, eficiencia y facilidad de uso. Nuestro enfoque en la calidad es
+                            inquebrantable, asegurando que cada artículo Newkool sea una inversión duradera para tu
+                            bienestar y comodidad. Creemos firmemente que la excelencia en cada detalle hace la
+                            diferencia en tu día a día.</p>
+                    </header>
+                    <div 
+                    :class="isBorder1Visible ? 'opacity-100 scale-100':'opacity-0 scale-0'"
+                    class="flex justify-center duration-500">
+                        <div class="space-y-4">
+                            <img src="/assets/route-images/somos-newkool/1-desktop.jpg" alt=""
+                                class="bg-neutral-200 p-4 rounded-xl duration-500 delay-200 hidden md:block ">
+                            <img
+                                src="/assets/route-images/somos-newkool/1-mobile.jpg" alt=""
+                                class="bg-neutral-200 p-4 h-fit rounded-xl duration-500 delay-300 block md:hidden ">
+                        </div>
                     </div>
-
-
-                </section>
-
-            </div>
-        </MainTemplate>
+                </div>
+            </section>
+            <section 
+                ref="target2"
+                :class="isTarget2Visible ? 'opacity-100 translate-x-0':'opacity-0 translate-x-full'" 
+                class=" flex justify-center py-20 duration-700 md:h-screen">
+                <div
+                class="flex flex-col md:flex-row-reverse items-center w-11/12">
+                    <div ref="border2" class="h-full w-0.5"></div>
+                    <header class="md:w-1/2 space-y-5 md:text-right">
+                        <div
+                            class="flex flex-row-reverse md:flex-row justify-end items-center text-newkool-red gap-5 w-10/12">
+                            <h2 class="font-bold text-5xl">Nuestra Misión</h2><svg
+                                class="svg-inline--fa fa-bullseye text-5xl p-3 rounded-full bg-gray-200"
+                                aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bullseye" role="img"
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                <path class="" fill="currentColor"
+                                    d="M448 256A192 192 0 1 0 64 256a192 192 0 1 0 384 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 80a80 80 0 1 0 0-160 80 80 0 1 0 0 160zm0-224a144 144 0 1 1 0 288 144 144 0 1 1 0-288zM224 256a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z">
+                                </path>
+                            </svg>
+                        </div>
+                        <p class="font-light md:w-10/12 text-gray-700"> Nuestra misión es enriquecer la vida de nuestros
+                            clientes a través de electrodomésticos innovadores y de la más alta calidad. Nos esforzamos
+                            por ofrecer soluciones que simplifiquen las tareas del hogar y mejoren la experiencia de
+                            vida. Complementamos esto con un servicio al cliente excepcional, garantizando la
+                            satisfacción y fomentando una lealtad duradera hacia nuestra marca. </p>
+                    </header>
+                    <div 
+                    :class="isBorder2Visible ? 'opacity-100 scale-100':'opacity-0 scale-0'"
+                    class="flex justify-center duration-500">
+                        <div class="space-y-4"><img src="/assets/route-images/somos-newkool/2-desktop.jpg" alt=""
+                                class="bg-neutral-200 p-4 rounded-xl duration-500 delay-200 hidden md:block "><img
+                                src="/assets/route-images/somos-newkool/2-mobile.jpg" alt=""
+                                class="bg-neutral-200 p-4 h-fit rounded-xl duration-500 delay-300 block md:hidden ">
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section 
+                ref="target3"
+                :class="isTarget3Visible ? 'opacity-100 translate-x-0':'opacity-0 -translate-x-full'" 
+                class=" flex justify-center py-20 duration-700 md:h-screen">
+                <div
+                
+                class="flex flex-col md:flex-row items-center w-11/12">
+                    <div ref="border3" class="h-full w-0.5"></div>
+                    <header class="md:w-1/2 space-y-5 md:text-right">
+                        <div
+                            class="flex flex-row-reverse md:flex-row justify-end items-center text-newkool-red gap-5 w-10/12">
+                            <h2 class="font-bold text-5xl">Nuestra Visión</h2><svg
+                                class="svg-inline--fa fa-lightbulb text-5xl py-3 px-4 rounded-full bg-gray-200"
+                                aria-hidden="true" focusable="false" data-prefix="fas" data-icon="lightbulb" role="img"
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                                <path class="" fill="currentColor"
+                                    d="M272 384c9.6-31.9 29.5-59.1 49.2-86.2l0 0c5.2-7.1 10.4-14.2 15.4-21.4c19.8-28.5 31.4-63 31.4-100.3C368 78.8 289.2 0 192 0S16 78.8 16 176c0 37.3 11.6 71.9 31.4 100.3c5 7.2 10.2 14.3 15.4 21.4l0 0c19.8 27.1 39.7 54.4 49.2 86.2H272zM192 512c44.2 0 80-35.8 80-80V416H112v16c0 44.2 35.8 80 80 80zM112 176c0 8.8-7.2 16-16 16s-16-7.2-16-16c0-61.9 50.1-112 112-112c8.8 0 16 7.2 16 16s-7.2 16-16 16c-44.2 0-80 35.8-80 80z">
+                                </path>
+                            </svg>
+                        </div>
+                        <p class="font-light md:w-10/12 text-gray-700"> Aspiramos a ser la marca líder en el mercado de
+                            electrodomésticos, reconocida no solo por la innovación y el diseño vanguardista de nuestros
+                            productos, sino, fundamentalmente, por la calidad superior que nos distingue. Visualizamos
+                            un futuro donde Newkool sea sinónimo de confianza y excelencia, apoyando el bienestar de las
+                            familias venezolanas a través de tecnología confiable y duradera. </p>
+                    </header>
+                    <div 
+                    :class="isBorder3Visible ? 'opacity-100 scale-100':'opacity-0 scale-0'"
+                    class="flex justify-center duration-500">
+                        <div class="space-y-4"><img src="/assets/route-images/somos-newkool/3-desktop.jpg" alt=""
+                                class="bg-neutral-200 p-4 rounded-xl duration-500 delay-200 hidden md:block "><img
+                                src="/assets/route-images/somos-newkool/3-mobile.jpg" alt=""
+                                class="bg-neutral-200 p-4 h-fit rounded-xl duration-500 delay-300 block md:hidden ">
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section 
+                ref="target4"
+                :class="isTarget4Visible ? 'opacity-100 translate-x-0':'opacity-0 translate-x-full'" 
+                class=" flex justify-center py-20 duration-700 md:h-screen">
+                <div
+                
+                class="flex flex-col md:flex-row-reverse items-center w-11/12 gap-4">
+                    <div ref="border4" class="h-full w-0.5"></div>
+                    <header class="md:w-1/2 space-y-5">
+                        <h1 class="font-bold text-newkool-red text-5xl w-10/12">Nuestros Valores</h1>
+                        <p class="font-light md:w-10/12 text-gray-700 text-left"><strong
+                                class="font-bold">Calidad:</strong><br> Es el pilar de todo lo que hacemos. Nos
+                            comprometemos a ofrecer productos que no solo sean funcionales y eficientes, sino que
+                            también estén construidos para perdurar, superando los más altos estándares de calidad en
+                            cada etapa. <br><br><strong class="font-bold">Innovación:</strong><br> Buscamos
+                            constantemente nuevas formas de mejorar y simplificar la vida de nuestros clientes a través
+                            de tecnología de vanguardia y diseños inteligentes. <br><br><strong
+                                class="font-bold">Compromiso:</strong><br> Estamos profundamente comprometidos con
+                            nuestros clientes y con la comunidad venezolana, esforzándonos por superar sus expectativas
+                            en cada interacción. <br><br><strong class="font-bold">Excelencia en el Servicio:
+                            </strong><br> Brindamos una atención al cliente excepcional, asegurando que cada experiencia
+                            con Newkool sea positiva y satisfactoria. <br><br><strong
+                                class="font-bold">Integridad:</strong><br> Actuamos con honestidad y transparencia en
+                            todas nuestras operaciones, construyendo relaciones basadas en la confianza y el respeto
+                            mutuo. </p>
+                    </header>
+                    <div 
+                    :class="isBorder4Visible ? 'opacity-100 scale-100':'opacity-0 scale-0'"
+                    class="flex justify-center duration-500">
+                        <div class="space-y-4"><img src="/assets/route-images/somos-newkool/4-desktop.jpg" alt=""
+                                class="bg-neutral-200 p-4 rounded-xl duration-500 delay-200 hidden md:block "><img
+                                src="/assets/route-images/somos-newkool/4-mobile.jpg" alt=""
+                                class="bg-neutral-200 p-4 h-fit rounded-xl duration-500 delay-300 block md:hidden ">
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+    </MainTemplate>
 </template>
 
 <style scoped></style>
-
